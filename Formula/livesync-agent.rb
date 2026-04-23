40084c970f7c78907a95b4b0212fbed78b7c4471
Aitor Roma <aitor@nimbox360.com>
2026-04-23 23:53:49 +0200

    Fix broken formula content and update checksum

--- Formula/livesync-agent.rb
+++ Formula/livesync-agent.rb
@@ -4,6 +4,17 @@ class LivesyncAgent < Formula
   license "MIT"
 
   url "https://github.com/aitorroma/obsidian-livesync-agent/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "1ee8648769126cc7f9acd2f182826e8fc5957c4a5bfc5d92f927237a446adca9")
+  sha256 "1ee8648769126cc7f9acd2f182826e8fc5957c4a5bfc5d92f927237a446adca9"
+
+  head "https://github.com/aitorroma/obsidian-livesync-agent.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "./")
+  end
+
+  test do
+    assert_match "livesync-agent", shell_output("#{bin}/livesync-agent --version")
   end
 end
