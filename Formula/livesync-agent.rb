e805045e2964310e374250e9670a8174f1946a95
Aitor Roma <aitor@nimbox360.com>
2026-04-23 23:53:40 +0200

    Fix checksum for v0.1.0 source tarball

--- Formula/livesync-agent.rb
+++ Formula/livesync-agent.rb
@@ -4,17 +4,6 @@ class LivesyncAgent < Formula
   license "MIT"
 
   url "https://github.com/aitorroma/obsidian-livesync-agent/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "be9c68a64f1375f619eb268754a8a8247d13f35044f9f5e69fe588cd00508db1"
-
-  head "https://github.com/aitorroma/obsidian-livesync-agent.git", branch: "main"
-
-  depends_on "rust" => :build
-
-  def install
-    system "cargo", "install", *std_cargo_args(path: "./")
-  end
-
-  test do
-    assert_match "livesync-agent", shell_output("#{bin}/livesync-agent --version")
+  sha256 "1ee8648769126cc7f9acd2f182826e8fc5957c4a5bfc5d92f927237a446adca9")
   end
 end
